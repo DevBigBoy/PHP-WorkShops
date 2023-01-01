4897de668837f504394734b8beb49a5c011f38fa
Mohamed Elshazly <56097354+DevMohamedElshazly@users.noreply.github.com>
2023-01-01 02:30:00 +0200

    Day001

---
+++ 001-Variables.php
@@ -0,0 +1,67 @@
+<?php
+/*
+*   Full Tag <?php ?>
+*   short Tag <? ?>
+*   ASP-like tag <% %>
+*/ 
+
+// Single line comment
+
+/*
+    Multiple Comments
+*/
+
+# shell Scriptinh style comment (not recommended)
+
+/* echo "
+    <style>
+        h1{
+            color: red;
+        }
+    </style>
+    <h1>Welcome to our website</h1>
+
+    <script>
+    alert('Welcome from php');
+    </script>
+    ";
+*/ 
+
+// for($i=0; $i < 10 ; $i++){
+//     echo "<p>hello<p>";
+// }
+// echo is much like a keyword than a function
+// print('hello From PHP');
+
+?>
+
+<?php
+// Variables
+// 25%
+?>
+
+<!-- Embedding php inside html -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome to php</title>
+
+    <style>
+        h1{
+            color: red;
+        }
+    </style>
+</head>
+<body>
+    <?php 
+        // echo "<h1>welcome to php</h1>";
+
+        // echo date('d/m/y H:i:s');
+
+
+    ?>
+</body>
+</html>
+
